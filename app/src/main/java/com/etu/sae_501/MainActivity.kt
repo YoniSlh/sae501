7197786cc925b456d617b81278915d73c56a6db6
dagon3u <jonathan.dagon1@etu.univ-lorraine.fr>
2024-11-05 11:30:18 +0100

    Ajout de la caméra, prise de photos et vidéos + affichage du rendu de la photo (?get à modifier car deprecated)

--- app/src/main/java/com/etu/sae_501/MainActivity.kt
+++ app/src/main/java/com/etu/sae_501/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.etu.sae_501
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView : ImageView
+    private lateinit var button : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +28,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        imageView = findViewById(R.id.image_save)
+        button = findViewById(R.id.btn_camera)
+
+        button.setOnClickListener{
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            try {
+                getResult.launch(takePictureIntent)
+            }catch (e: ActivityNotFoundException){
+                Toast.makeText(this, "Erreur lors du démarrage de la caméra : " + e.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val imageBitmap = it.data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
         }
     }
 }
