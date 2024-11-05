91fdab2780c35f52c5867a6777eadc0062bc5f64
Illia Linkevych (flushion) <linkevich594@gmail.com>
2024-11-05 13:10:19 +0100

    Intégration de Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,18 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
 }
 
 dependencies {
-
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
